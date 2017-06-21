75df292ec3b31c6bb5c0704754e5e942d7de1c88
Tyler Davis <tyjdavis@outlook.com>
2017-06-21 14:51:59 -0400

    added more button

--- js/init.js
+++ js/init.js
@@ -6,3 +6,12 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+//more button in html
+$('a.read_more').click(function(e){
+  // console.log("test");
+    e.preventDefault(); //to prevent standard click event
+    $('.more_text').toggle();
+});
